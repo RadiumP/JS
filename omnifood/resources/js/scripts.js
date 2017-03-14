10b46c42ab2cf05d69721adf47c19864fb13cd8a
RadiumP <chenyan5@asu.edu>
2017-03-13 20:51:31 -0700

    add some jquery navi effects

--- omnifood/resources/js/scripts.js
+++ omnifood/resources/js/scripts.js
@@ -1,3 +1,43 @@
 $(document).ready(function(){
    
+    //scroll down show navi
+    $('.js--section-features').waypoint(function(direction){
+      if(direction == "down")
+          {
+              $('nav').addClass('sticky');
+          }else{
+              $('nav').removeClass('sticky');
+          }
+   }, {
+       offset: '60px'
+   });
+   
+    //scroll down when click
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);//1000ms
+    });
+    
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);//1000ms
+    });
+    
+    
+    //navi scroll
+    //smooth scrolling from css-tricks
+    $(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+    
+    
 });
